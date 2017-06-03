7726e8f447ce7064b71569c4fcece1f008770541
Stephen Chung <spchung@andrew.cmu.edu>
2017-06-03 19:22:54 -0400

    be able to focus on opened soundcloud tab

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.commands.onCommand.addListener(function (command) {
       chrome.tabs.create({url : "http://google.com" });
       break;
     case "open-soundcloud" :
-      console.log("opening soundcloud");
+      // console.log("opening soundcloud");
       var query = {
         "url" : "*://soundcloud.com/*"
       }
@@ -13,8 +13,25 @@ chrome.commands.onCommand.addListener(function (command) {
         if (soundcloudTabs.length == 0) {
           chrome.tabs.create({ url : "https://soundcloud.com" });
         } else {
-          var tab = soundcloudTabs[0]; // TODO: determine which active tab to switch to. For now, just go to the first soundcloud tab that is already open
-          
+          console.log("some tabs already open, switch to that tab");
+          var tabToSwitchTo = null;
+          var audibleTabs = soundcloudTabs.filter(function (t) {
+            return t.audible == true;
+          });
+          if (audibleTabs.length > 0) {
+            tabToSwitchTo = audibleTabs[0]; // still need to do a better switching
+          } else {
+            tabToSwitchTo = soundcloudTabs[0];
+          }
+          var windowUpdateInfo = {
+            "focused" : true
+          };
+          var tabUpdateInfo = {
+            "active" : true
+          };
+          chrome.windows.update(tabToSwitchTo.windowId, windowUpdateInfo);
+          // TODO: not all tabs will have an id.
+          chrome.tabs.update(tabToSwitchTo.id,tabUpdateInfo);
         }
       });
     default: break;
