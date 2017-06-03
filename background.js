455646207fa8d04796c176dfde6295dca5dbe181
Stephen Chung <spchung@andrew.cmu.edu>
2017-06-03 18:24:13 -0400

    be able to open soundcloud.com via shortcut if a tab isn't open yet

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
+// Keyboard shortcuts for this extension
 chrome.commands.onCommand.addListener(function (command) {
-  if (command === "open-player") {
-    chrome.tabs.create({url : "http://google.com" });
+  switch (command) {
+    case "open-player" :
+      chrome.tabs.create({url : "http://google.com" });
+      break;
+    case "open-soundcloud" :
+      console.log("opening soundcloud");
+      var query = {
+        "url" : "*://soundcloud.com/*"
+      }
+      chrome.tabs.query(query,function(soundcloudTabs) {
+        if (soundcloudTabs.length == 0) {
+          chrome.tabs.create({ url : "https://soundcloud.com" });
+        } else {
+          var tab = soundcloudTabs[0]; // TODO: determine which active tab to switch to. For now, just go to the first soundcloud tab that is already open
+          
+        }
+      });
+    default: break;
   }
 });
